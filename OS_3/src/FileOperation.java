8a3ef603d76988b48c5c85d9eb08fb94ddf551cb
s4jeherr <s4jeherr@uni-trier.de>
2025-03-17 15:56:30 +0100

    Delete OS_3 directory

--- OS_3/src/FileOperation.java
+++
@@ -1,6 +0,0 @@
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-
-public interface FileOperation {
-    void apply() throws IOException, NoSuchAlgorithmException;   // Execute the operation
-}
